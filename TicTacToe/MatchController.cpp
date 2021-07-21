1bfcee7084ee890f41a2359f5694b71976f5fd79
Keegan <s200322@students.aie.edu.au>
2021-07-21 04:42:44 +0000

    Added getter for the players

--- TicTacToe/MatchController.cpp
+++ TicTacToe/MatchController.cpp
@@ -1,1 +1,25 @@
 #include "MatchController.h"
+
+/// <summary>
+/// Constructor to setup game
+/// </summary>
+/// <param name="p1">Player 1</param>
+/// <param name="p2">Player 2</param>
+/// <param name="num_of_rnds">How many rounds we are playing</param>
+MatchController::MatchController(Player * const p1, Player * const p2, int num_of_rnds)
+{
+	Player1 = p1;
+	Player2 = p2;
+	numOfRnds = num_of_rnds;
+}
+
+MatchController::~MatchController()
+{
+	delete Player1;
+	delete Player2;
+}
+
+void MatchController::GameOver()
+{
+	// TODO: implement the game over screen that shows the winner
+}
--- TicTacToe/MatchController.h
+++ TicTacToe/MatchController.h
@@ -1,5 +1,46 @@
+/**********
+* Match Controller Header
+* Handles the current match
+* Keegan Miller | AIE | Tic Tac Toe
+***********/
 #pragma once
+#include "Player.h"
 class MatchController
 {
+public:
+	MatchController(Player* const p1, Player* const p2, int num_of_rnds);
+	~MatchController();
+
+#pragma region Methods
+
+
+	inline void EndRound() { rndsPlayed++; }
+	void GameOver();
+
+#pragma endregion
+
+#pragma region Getters & Setters
+	inline Player* GetPlayer1() const { return Player1; }
+	inline Player* GetPlayer2() const { return Player2; }
+#pragma endregion
+
+
+	
+private:
+
+#pragma region Properties
+
+
+	Player* Player1;
+	Player* Player2;
+
+	int numOfRnds = 5;
+	int rndsPlayed = 0;
+
+
+#pragma endregion
+
+	
+
 };
 
